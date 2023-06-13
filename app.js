d0df63bbef8ad67943d0e0c08798542993859b30
Moh_Sinab <user@users-MacBook-Air.local>
2023-06-13 20:20:38 +0530

    update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
-
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const  mongoose = require("mongoose");
