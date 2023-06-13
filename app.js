f0d67ab931788d3966933853acc1e2ab09a32545
Moh_Sinab <user@users-MacBook-Air.local>
2023-06-13 12:37:16 +0530

    Intial commit

---
+++ app.js
@@ -0,0 +1,80 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const  mongoose = require("mongoose");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://sinabnc:FIfa2018@cluster0.mbjfdqg.mongodb.net/todo");
+
+const itemSchema={
+  name:String
+}
+
+const Item=mongoose.model("Item",itemSchema)
+
+const item1=new Item({
+  name:"welcome to Tosolist"
+})
+const item2=new Item({
+  name:"Hit + button to add"
+})
+const item3=new Item({
+  name:"Hit delete to delete"
+})
+
+const defaultItem=[item1,item2,item3]
+// Item.insertMany(defaultItem).then(()=>console.log("success")).catch((err)=>console.log(err))
+
+
+app.get("/", function(req, res) {
+
+  Item.find({}).then((foundItem)=>{
+    if(foundItem.length===0){
+Item.insertMany(defaultItem).then(()=>console.log("success")).catch((err)=>console.log(err))
+res.redirect("/")
+    }else{
+      res.render("list", {listTitle: day, newListItems: foundItem})
+    }
+  })
+  
+const day = date.getDate();
+
+
+});
+
+app.post("/", function(req, res){
+
+  const item = req.body.newItem;
+  const itemName=new Item({
+    name:item
+  })
+  itemName.save()
+  res.redirect("/")
+});
+
+app.post("/delete",(req,res)=>{
+  const box=req.body.worngBox;
+  Item.findOneAndRemove(box).then(()=>console.log("worked")).catch((err)=>console.log(err))
+  res.redirect("/")
+})
+
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
